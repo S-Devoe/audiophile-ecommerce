e1d65cca19cc3df43b4abefe02de5363bf63d9cb
Lagbalu Lateef <chelseg14@gmail.com>
2022-05-28 15:01:19 +0100

    modified some styles

--- components/molecules/MobileNav.tsx
+++ components/molecules/MobileNav.tsx
@@ -6,6 +6,13 @@ import { useDispatch, useSelector } from 'react-redux'
 function MobileNav() {
   const navOpen = useSelector(isNavOpen)
   const dispatch = useDispatch()
+
+  if (navOpen){
+   document.body.style.overflowY = "hidden";
+  } else{
+    document.body.style.overflowY = "visible";
+  }
+  // const
   return (
     <nav className="mobile_nav">
       <div className={`active_mobile-nav ${navOpen ? "active" : "default"}`}>
